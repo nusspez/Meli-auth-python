b4b55e2a04796402fc9abe9c44fcdf1d0ebd9c81
peznuss <peznuss@MacBook-Air-de-peznuss.local>
2021-02-20 03:41:50 -0600

    make it better

--- app.py
+++ app.py
@@ -46,20 +46,6 @@ def show_user_profile():
     decode_response = response.read().decode()
     return jsonify(decode_response) 
 
-@app.route('/home', methods=["GET", "POST"])
-def home():
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'Do Something':
-            client_id = os.getenv('CLIENT_ID')
-            redirect_uri = os.getenv('REDIRECT_URI')
-            url = f"http://auth.mercadolibre.com.mx/authorization?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}"
-            req = urllib.request.Request(url)
-            f = urllib.request.urlopen(req) 
-            the_page = f.geturl()
-            return redirect(str(the_page), code=302)
-
-    return render_template("home.html")
-
 
 if __name__ == '__main__':
     app.run(port=PORT, debug=DEBUG)
